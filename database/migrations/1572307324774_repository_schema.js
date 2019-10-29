b1d9e25cf5ebad89d50698f58ac6a7361874bf00
Smith Júnior <smith.junior@icloud.com>
2019-10-28 21:55:06 -0300

    Migrations and docker configurate

--- database/migrations/1572307324774_repository_schema.js
+++ database/migrations/1572307324774_repository_schema.js
@@ -4,14 +4,31 @@
 const Schema = use('Schema')
 
 class RepositorySchema extends Schema {
-  up () {
+  up() {
     this.create('repositories', (table) => {
-      table.increments()
+      table.bigIncrements()
+      table.string('name')
+      table.string('full_name').unique()
+      table.string('html_url').unique()
+      table.string('homepage')
+      table.bool('private')
+      table.string('description')
+      table.string('owner_login')
+      table.string('owner_gravatar')
+      table.string('owner_html_url')
+      table.string('license_key')
+      table.string('license_url')
+      table.string('open_issues')
+      table.datetime('creation_datetime')
+      table.datetime('last_update_datetime')
+      table.string('language')
+      table.integer('forks_count')
+      table.string('watchers')
       table.timestamps()
     })
   }
 
-  down () {
+  down() {
     this.drop('repositories')
   }
 }
